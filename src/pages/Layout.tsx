53541689be97969c463f162d89e61d69287c0775
David Perez <perez.david3001@gmail.com>
2023-10-19 21:27:53 -0500

    Setup routing

---
+++ src/pages/Layout.tsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "../components/NavBar";
+
+/* A layout component is a component that wraps other components and provides a common layout for them.
+In this case, we want to wrap our NavBar component around all of our pages.
+We can do this by using the Outlet component from react-router-dom.
+The Outlet component is a placeholder for where the child routes will be rendered.
+We can then render the NavBar component above the Outlet component.
+This will render the NavBar component on every page that is rendered inside of the Outlet component. */
+export const Layout = () => {
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+    </>
+  );
+};
