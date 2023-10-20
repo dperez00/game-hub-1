82087972586debc79d2d62c88a2b3590528a35e2
David Perez <perez.david3001@gmail.com>
2023-10-19 21:58:54 -0500

    Handle errors

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage";
 import GameDetailPage from "./pages/GameDetailPage";
 import HomePage from "./pages/HomePage";
 import { Layout } from "./pages/Layout";
 
+// Only the children will be rendered inside the Outlet.
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       { path: "games/:id", element: <GameDetailPage /> },
